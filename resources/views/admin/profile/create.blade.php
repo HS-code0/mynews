5a7c562d683d67fbbd1de0da6ef2860e53133481
iamhiroyukisuzuki@gmail.com <iamhiroyukisuzuki@gmailm.com>
2021-12-12 13:02:28 +0000

    PHPLaravel1課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,48 +1,17 @@
-!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible"content="IE=edge">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        <meta nmae="csrf-token" content="{{ csrf_token()}}">
-        <title>@section('title')</title>
-        <script src="{{ secure_asset('js/app.js') }}"defer></script>
-        <link rel="dns-prefetch"href="http://fonts.gstatic.com">
-        <link href="http://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet"type="text/css">
-        <link href="{{ secure_asset('css/app.css') }}"rel="stylesheet">
-        <link href="{{ secure_asset('css/profile.css') }}"rel="stylesheet">
-    </head>
-    <body>
-        <div id="app">
-            <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
-                @section('content')
-                <div class="container">
-                    <div class="row">
-                        <div class="container">
-                           <div class="col-md-8 mx-auto">
-                                <h2>My プロフィール</h2>
-           <a class="navbar-brand href="{{url('/') }}">
-                    </a>
-                    <button class="navbar-toggler"type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-
-                    controls="navbarSupportedContent" aria-expanded="false"aria-label="Toggle nabigation">
-                    <span class="navbar-toggler-icon">
-                    </span>
-                    </button>
-                    <div class="collapse navbar-collapse"id="navbarSupportedContent">
-                        <ul ckass="navbar-nav mr-auto">
-                        </ul>
-                        <ul class="navbar-nav ml-auto">
-                        </ul>
-                    </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>    
-            </nav>
-            <main class="py-4">
-                @yield('content')
-                @section('title', 'プロフィールページ')
-            </main>
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'プロフィール画面')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+            </div>
         </div>
-    </body>
-</html>
+    </div>
+@endsection
